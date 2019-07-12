1e0915c44e79433e96d97f1ae0827bdc8b43c5f1
elizhang227 <zhang.eli@hotmail.com>
2019-07-12 09:25:46 -0400

    hard coded some params

--- routes/version1.js
+++ routes/version1.js
@@ -42,13 +42,17 @@ router.put('/update/:post_id?', async (req, res) => {
 })
 
 router.post('/post/add', async (req, res) => {
-    const { title, author_id, content } = req.body;
+    //const { title, author_id, content } = req.body;
+    const { title, content } = req.body;
+    const author_id = 1;
     const response = await postModel.addPost(title, content, author_id);
+    console.log('this is response', response)
     if (response.command === 'INSERT' && response.rowCount >= 1) {
-        res.sendStatus(200)
+        res.redirect('http://localhost:3001')
     } else {
         res.send(`Could not add new blog post ${title}`).status(409)
     }
+    
 })
 
 module.exports = router;
